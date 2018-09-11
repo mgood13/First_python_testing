6e25f89d0a33cf559da1afa485dbaf8a180566db
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-09-11 16:17:18 -0400

    Created a new file that adds three numbers then divides by 3

---
+++ Add_Divide.py
@@ -0,0 +1,26 @@
+def main():
+	sum = add_three(15, 2, 31)
+	divide_by_three(sum)
+
+def add_three(v1, v2, v3):
+	"""This function adds three numbers together
+	:param v1: First number
+	:param v2: Second number
+	:param v3: Thirds number
+	"""
+
+	p = v1 + v2 + v3
+	return p
+
+def divide_by_three(v1):
+	"""This function divides a number by 3
+	:param v1: First Number
+	:returns: product number
+	"""
+
+	p = v1/3
+	print(p)
+
+
+if __name__ == "__main__":
+	main()
