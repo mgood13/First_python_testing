9af07094bcbf105c5674ee7ec4ef6d12b2f29d18
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-09-20 16:05:50 -0400

    Adds the testing file test_Add_divide

---
+++ test_Add_Divide.py
@@ -0,0 +1,14 @@
+def test_add_three()
+        """This tests the function which is intended to add three numbers together.
+        """
+        from Add_Divide import add_three
+        s = add_three(1,2,3)
+        assert s == 6
+
+def test_divide_by_three()
+        """This tests the function which is intended to divide a number by three.
+        """
+	from Add_Divide import divide_by_three
+	d = divide_by_three(3)
+	assert d == 1
+
